c6553198ea6dc6fe29f3fdec1769e24e2c5f771b
Michael Gerdemann <me+github@gerdemann.me>
2022-05-13 20:12:15 +0200

    feat: initial commit

---
+++ Readme.md
@@ -0,0 +1,41 @@
+# HTTP Status Code
+Check an url for http status codes in your Github Actions.
+
+## Usage
+
+Create your Github Workflow configuration in `.github/workflows/http_status.yml` or similar.
+
+Example: [http_status.yml](.github/workflows/http_status.yml)
+
+```yml
+name: Workflow for checking http status codes
+
+on:
+  push:
+    branches: [ main ]
+    tags: [ '*.*.*' ]
+
+jobs:
+  build:
+    name: HTTP Status
+    runs-on: ubuntu-latest
+    steps:
+      # ... uses ....
+      - name: Check HTTP status
+        uses: gerdemann/http-status-code@1.0.0
+        with:
+          url: https://github.com
+          code: 200
+          timeout: 60
+          interval: 10
+      # ... uses ....
+```
+
+## Inputs
+
+The following configuration options are available:
+
+* `url` The url to check (e.g. "https://github.com")
+* `code` The HTTP status code (eg: 200)
+* `timeout` Timeout before giving up in seconds
+* `interval` Interval between polling in seconds
---
+++ main.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "Polling endpoint for status"
+      echo " "
+      echo "./main.sh [options]"
+      echo " "
+      echo "options:"
+      echo "-h, --help           Show brief help"
+      echo "--url=URL            url to poll"
+      echo "--interval=INTERVAL  Interval between each call, in seconds"
+      echo "--timeout=TIMEOUT    Timeout before stop polling, in seconds"
+      exit 0
+      ;;
+    --url*)
+      url=`echo $1 | sed -e 's/^[^=]*=//g'`
+      shift
+      ;;
+    --code*)
+      code=`echo $1 | sed -e 's/^[^=]*=//g'`
+      shift
+      ;;
+    --interval*)
+      interval=`echo $1 | sed -e 's/^[^=]*=//g'`
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+function poll_status {
+  while true;
+  do
+    STATUS_CODE=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" $url -o /dev/null`
+    echo "$(date +%H:%M:%S): The status code is $STATUS_CODE";
+    if [[ "$STATUS_CODE" == "200" ]]; then
+          echo "success";
+          exit 0;
+        break;
+    fi;
+    sleep $interval;
+  done
+}
+
+printf "\nPolling '${url%\?*}' every $interval seconds, until status is '200'\n"
+poll_status
